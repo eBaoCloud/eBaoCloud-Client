243b10ebac48edf283a5fa58061f5529d4a8b572
Jie Cheng <jie.cheng@ebaotech.com>
2017-04-16 17:00:06 +0800

    support policy document upload

--- com.ebao.gs.ebaocloud.sea.client/pub/NetworkUtils.cs
+++ com.ebao.gs.ebaocloud.sea.client/pub/NetworkUtils.cs
@@ -55,31 +55,71 @@ namespace com.ebao.gs.ebaocloud.sea.seg.client.pub
             JObject responseObj = JObject.Parse(httpResponse.RawText);
             return responseObj;
         }
-        
-        public static JObject UploadFile(string path, FileInfo file, UploadFileParams parameters = null, string token = null)
-        {
-       
-            HttpClient httpClient = new HttpClient(host);
-            httpClient.Request.Accept = HttpContentTypes.ApplicationJson;
-            httpClient.Request.AddExtraHeader("Authorization", "Basic " + token);
 
-            UploadFileParams parameter = new UploadFileParams();
-            parameter.uploadExtraData = new UploadFileExtraData();
-            parameter.uploadExtraData.policyId = "1111";
-            parameter.uploadExtraData.docName = "doc";
-            parameter.uploadExtraData.docType = "1";
-           parameter.fileStream = new FileStream(file.FullName, FileMode.OpenOrCreate);
-            HttpResponse httpResponse = httpClient.Post(path, parameters, token);
-            requestSuccess(httpResponse);
-            JObject responseObj = JObject.Parse(httpResponse.RawText);
-            return responseObj;
+		public static JObject UploadFile(string path, UploadFileParams parameters = null, string token = null)
+		{
+			WebClient webClient = new WebClient();
+			System.Net.WebRequest webRequest = System.Net.WebRequest.Create(host + path);
+			string boundary = "----------------------------" + DateTime.Now.Ticks.ToString("x");
+			webRequest.Method = "POST";
+			webRequest.Headers.Add("Authorization", "Basic " + token);
+			webRequest.ContentType = "multipart/form-data; boundary=" + boundary;
+			Stream postDataStream = GetPostStream(parameters.fileInfo, parameters.uploadExtraData, boundary);
+			webRequest.ContentLength = postDataStream.Length;
+			Stream reqStream = webRequest.GetRequestStream();
+			postDataStream.Position = 0;
+			byte[] buffer = new byte[1024];
+			int bytesRead = 0;
+			while ((bytesRead = postDataStream.Read(buffer, 0, buffer.Length)) != 0)
+			{
+				reqStream.Write(buffer, 0, bytesRead);
+			}
+			postDataStream.Close();
+			reqStream.Close();
+			string result;
+			try
+			{
+				StreamReader streamReader = new StreamReader(webRequest.GetResponse().GetResponseStream());
+				result = streamReader.ReadToEnd();
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+			return JsonConvert.DeserializeObject<JObject>(result);
+		}
 
+		private static Stream GetPostStream(FileInfo fileInfo, JObject uploadExtraData, string boundary)
+		{
+			Stream postDataStream = new System.IO.MemoryStream();
+			//adding form data
+			string formDataHeaderTemplate = Environment.NewLine + "--" + boundary + Environment.NewLine +
+			"Content-Disposition: form-data; name=\"{0}\";" + Environment.NewLine + Environment.NewLine + "{1}";
+			byte[] formItemBytes = System.Text.Encoding.UTF8.GetBytes(string.Format(formDataHeaderTemplate, "uploadExtraData", JsonConvert.SerializeObject(uploadExtraData, Formatting.Indented)));
+			postDataStream.Write(formItemBytes, 0, formItemBytes.Length);
 
+			//add fileData
+			string fileHeaderTemplate = Environment.NewLine + "--" + boundary + Environment.NewLine +
+			"Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"" +
+			Environment.NewLine + "Content-Type: \"{2}\"" + Environment.NewLine + Environment.NewLine;
 
-     
-       }
+			byte[] fileHeaderBytes = System.Text.Encoding.UTF8.GetBytes(string.Format(fileHeaderTemplate, "fileData", fileInfo.FullName, MimeMapping.GetMimeMapping(fileInfo.FullName)));
+			postDataStream.Write(fileHeaderBytes, 0, fileHeaderBytes.Length);
+			FileStream fileStream = fileInfo.OpenRead();
+			byte[] buffer = new byte[1024];
+			int bytesRead = 0;
+			while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+			{
+				postDataStream.Write(buffer, 0, bytesRead);
+			}
+			fileStream.Close();
+			byte[] endBoundaryBytes = System.Text.Encoding.UTF8.GetBytes("--" + boundary + "--");
+			postDataStream.Write(endBoundaryBytes, 0, endBoundaryBytes.Length);
 
-        static private void requestSuccess(HttpResponse resp)
+			return postDataStream;
+		}
+
+		static private void requestSuccess(HttpResponse resp)
         {
             if (resp.StatusCode != System.Net.HttpStatusCode.OK)
             {
