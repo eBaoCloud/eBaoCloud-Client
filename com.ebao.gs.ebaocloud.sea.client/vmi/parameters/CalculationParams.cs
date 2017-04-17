1ffbb7de907a653769969d6267bf8ef1d7f77b1d
leonluo <leonluo@LEONLUO7A32>
2017-04-17 13:43:22 +0800

    update baseModel

--- com.ebao.gs.ebaocloud.sea.client/vmi/parameters/CalculationParams.cs
+++ com.ebao.gs.ebaocloud.sea.client/vmi/parameters/CalculationParams.cs
@@ -2,25 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using com.ebao.gs.ebaocloud.sea.seg.client.pub;
 
 namespace com.ebao.gs.ebaocloud.sea.seg.client.vmi.parameters
 {
-    public class CalculationParams
+    public class CalculationParams : BaseModel
     {
-        public String productCode;
-        public String planCode;
-        public DateTime proposalDate;
-        public DateTime effectiveDate;
-        public DateTime expireDate;
+        public String productCode { get; set; }
+        public String planCode { get; set; }
+        public DateTime proposalDate { get; set; }
+        public DateTime effectiveDate { get; set; }
+        public DateTime expireDate { get; set; }
 
-        public String vehicleMakeName;
-        public int vehicleModelYear;
-        public String vehicleModelDescription;
-        public int vehicleRegistrationYear;
-        public Decimal vehicleAccessaryValue;
-        public Decimal vehicleTotalValue;
-        public VehicleUsage vehicleUsage;
-        public VehicleGarageType vehicleGarageType;
+        public String vehicleMakeName { get; set; }
+        public int vehicleModelYear { get; set; }
+        public String vehicleModelDescription { get; set; }
+        public int vehicleRegistrationYear { get; set; }
+        public Decimal vehicleAccessaryValue { get; set; }
+        public Decimal vehicleTotalValue { get; set; }
+        public VehicleUsage vehicleUsage { get; set; }
+        public VehicleGarageType vehicleGarageType { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 
     public enum VehicleUsage
